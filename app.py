aa757a79a266e85634572b3c9081e78bee261efd
Felipedjsdv <felipeliveiptv@gmail.com>
2025-07-17 00:48:50 -0300

    3 python

--- app.py
+++ app.py
@@ -103,5 +103,8 @@ def sair():
     session.clear()
     return redirect('/')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # usa porta fornecida pela Render
+    app.run(host="0.0.0.0", port=port)
